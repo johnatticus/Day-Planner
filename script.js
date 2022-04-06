186877bab1583ca7473c7e65a075545379a4d3fd
John Hysong <johnahysong@gmail.com>
2022-04-06 19:45:07 -0400

    added more to js. trying to get local storage to work

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 // variable to use moment js to put the date and time on html
-var timeNdate = moment().format("MMMM Do YYYY, h:mm:ss a")
+var timeNdate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
 $("#todaysdatetime").text(timeNdate);
 
 // function to update the above timeNdate variable every second
 var update = function() {
-    document.getElementById("todaysdatetime").innerHTML = moment().format("MMM do YYYY, h:mm:ss a")
+    document.getElementById("todaysdatetime").innerHTML = moment().format("dddd, MMMM do YYYY, h:mm:ss a")
 }
-
 setInterval(update, 1000)
 
-// $(document).ready(function() {
+// start a jquery function
+$(document).ready(function() {
 
     $("saveBtn").on("click", function() {
-    var value = $(this).sibling(".information").val()
+    var value = $(this).siblings(".information").val()
     var time = $(this).parent().attr("id")
     localStorage.setItem(time, value)
-    }) 
+    })
 
-function updateColor() {
-    var now = new Date().getHours()
-    if (now > 9) {
-        $("#9am").addClass("past")
-    }   else if (now >= 9 && now <10) {
-        $("#9am").addClass("present")
-    }   else {
-        $("#9am").addClass("future")
-    }            
-}
+    function colorUpdate()
+    // var currentHour = moment().hours();
+    // $(".time-block").each(function() {
+    //     // create sometime to loop over time blocks
+    // var blockTime =
+    // )}
+
+    
+    // need saved data from local storage
+    $("#9am .information").val(localStorage.getItem("9am"))
+    $("#10am .information").val(localStorage.getItem("10am"))
+    $("#11am .information").val(localStorage.getItem("11am"))
+    $("#12pm .information").val(localStorage.getItem("12pm"))
+    $("#12pm .information").val(localStorage.getItem("1pm"))
+    $("#12pm .information").val(localStorage.getItem("2pm"))
+    $("#12pm .information").val(localStorage.getItem("3pm"))
+    $("#12pm .information").val(localStorage.getItem("4pm"))
+    $("#12pm .information").val(localStorage.getItem("5pm"))
+});
