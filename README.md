ca13d2c537411c40c8120c1c9d2445665831a617
John Hysong <johnahysong@gmail.com>
2022-04-07 01:19:11 -0400

    work on readme.md and added screenshot

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Day-Planner
-A simple day planner
+
+![day planner screenshot](screenshot.png)
+## A simple day planner
+*created by John Hysong*
+
+A day planner made using jquery, bootstrap and moment js.
+
+- used local storage to save entries so they remain when the page is refreshed or opened again in the future
+- uses moment js to place current time
+- jquery and moment js used to determine the color coding of the planner based on the time of day.
+
--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-// variable to use moment js to put the date and time on html
+// variable created that equals the current time and date via moment js
 var timeNdate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
+// writes that created variable to the html
 $("#todaysdatetime").text(timeNdate);
 
 // function to update the above timeNdate variable every second
 var update = function() {
     document.getElementById("todaysdatetime").innerHTML = moment().format("dddd, MMMM do YYYY, h:mm:ss a")
 }
+// sets the time displayed to update every second
 setInterval(update, 1000)
 
 // start a jquery function
 $(document).ready(function() {
 
     // function to make info save to local storage when the save button is pressed
+    // listens for click on the save button to run the function
+    // sets variables for time and value for the info entered by user
+    // saves them to local storage
     $(".saveBtn").on("click", function() {
     var value = $(this).siblings(".information").val()
     var time = $(this).parent().attr("id")
@@ -19,22 +24,29 @@ $(document).ready(function() {
     console.log(value)
     })
 
-    //set a constant that encompasses all rows in the html for all the business hours
+    // set a constant that encompasses all rows in the html for all the business hours
+    // since all arrays 
     const rows = document.getElementsByClassName("row");
     
     // sets currentHour to the local time hour with moment js api
     let currentHour = moment().hours();
     console.log(currentHour)
     
+    // takes the array of all the rows and runs the following code for each
     Array.from(rows).forEach(row => {
-      let rowIdString = row.id //, rowHour;
+        // lets the variable be the id of the of row
+        let rowIdString = row.id
         console.log(row.id)
         console.log(rowIdString)
-      if (rowIdString) {
+        
+        // if true it will parse the iteger of the variable
+        // when true, runs through the ifs to set the proper color for the row based on time
+        // by comparing the integer id of the row versus the real current time
+        if (rowIdString) {
         rowHour = parseInt(rowIdString);
         console.log(rowHour)
-      }
-      if (rowHour) {
+        }
+        if (rowHour) {
         if (currentHour === rowHour) {
             $(".information").addClass("present")
         } else if (currentHour < rowHour) {
@@ -47,6 +59,7 @@ $(document).ready(function() {
     
 
     // pulls any local storage to populare the planner when it is refreshed or re-opened
+    // this information is populated by the save button earlier in the script
     $("#9 .information").val(localStorage.getItem("9"))
     $("#10 .information").val(localStorage.getItem("10"))
     $("#11 .information").val(localStorage.getItem("11"))
