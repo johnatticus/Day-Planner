c0337c14a4f13615e3101c22055b58a32a7e0d8b
John Hysong <johnahysong@gmail.com>
2022-04-07 23:19:58 -0400

    worked on code in order to get present hour to show up as red

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var timeNdate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
 // writes that created variable to the html
 $("#todaysdatetime").text(timeNdate);
 
+
+
 // function to update the above timeNdate variable every second
 var update = function() {
     document.getElementById("todaysdatetime").innerHTML = moment().format("dddd, MMMM do YYYY, h:mm:ss a")
@@ -12,7 +14,6 @@ setInterval(update, 1000)
 
 // start a jquery function
 $(document).ready(function() {
-
     // function to make info save to local storage when the save button is pressed
     // listens for click on the save button to run the function
     // sets variables for time and value for the info entered by user
@@ -22,53 +23,38 @@ $(document).ready(function() {
     var time = $(this).parent().attr("id")
     localStorage.setItem(time, value)
     console.log(value)
+
+    hourColors()
     })
 
     // set a constant that encompasses all rows in the html for all the business hours
     // since all arrays 
-    const rows = document.getElementsByClassName("row");
+    // const rows = document.getElementsByClassName("row");
+// sets currentHour to the local time hour with moment js api
+    let currentHour = 11 //moment().hours();
     
-    // sets currentHour to the local time hour with moment js api
-    let currentHour = moment().hours();
-    console.log(currentHour)
-    
-    // takes the array of all the rows and runs the following code for each
-    Array.from(rows).forEach(row => {
-        // lets the variable be the id of the of row
-        let rowIdString = row.id
-        console.log(row.id)
-        console.log(rowIdString)
-        
-        // if true it will parse the iteger of the variable
-        // when true, runs through the ifs to set the proper color for the row based on time
-        // by comparing the integer id of the row versus the real current time
-        if (rowIdString) {
-        rowHour = parseInt(rowIdString);
-        console.log(rowHour)
+    function hourColors() {
+        for (let i = 9; i < 18; i++) {
+            const element = document.getElementById(`${i}`)
+            if (currentHour > i) {
+                $(element).addClass("past")
+            } else if (currentHour < i) {
+                $(element).addClass("future")
+            } else {
+                $(element).addClass("present")
+            }
         }
-        if (rowHour) {
-        if (currentHour === rowHour) {
-            $(".information").addClass("present")
-        } else if (currentHour < rowHour) {
-            $(".information").addClass("future")
-        } else if (currentHour > rowHour) {
-            $(".information").addClass("past")
-        } 
-      }
-    });
-    
+    }
 
     // pulls any local storage to populare the planner when it is refreshed or re-opened
-    // this information is populated by the save button earlier in the script
-    $("#9 .information").val(localStorage.getItem("9"))
-    $("#10 .information").val(localStorage.getItem("10"))
-    $("#11 .information").val(localStorage.getItem("11"))
-    $("#12 .information").val(localStorage.getItem("12"))
-    $("#13 .information").val(localStorage.getItem("13"))
-    $("#14 .information").val(localStorage.getItem("14"))
-    $("#15 .information").val(localStorage.getItem("15"))
-    $("#16 .information").val(localStorage.getItem("16"))
-    $("#17 .information").val(localStorage.getItem("17"))
-    
+// this information is populated by the save button earlier in the script
+$("#9 .information").val(localStorage.getItem("9"))
+$("#10 .information").val(localStorage.getItem("10"))
+$("#11 .information").val(localStorage.getItem("11"))
+$("#12 .information").val(localStorage.getItem("12"))
+$("#13 .information").val(localStorage.getItem("13"))
+$("#14 .information").val(localStorage.getItem("14"))
+$("#15 .information").val(localStorage.getItem("15"))
+$("#16 .information").val(localStorage.getItem("16"))
+$("#17 .information").val(localStorage.getItem("17"))
 });
-
