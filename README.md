406b2abaf42ed49e70c4322154831ecc218539c1
John Hysong <johnahysong@gmail.com>
2022-04-08 00:00:02 -0400

    updated screenshot. added links to readme.md. cleaned up js and comments

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ A day planner made using jquery, bootstrap and moment js.
 - uses moment js to place current time
 - jquery and moment js used to determine the color coding of the planner based on the time of day.
 
+[Repo](https://github.com/johnatticus/Day-Planner)
+
+[Live Link](https://johnatticus.github.io/Day-Planner/)
--- script.js
+++ script.js
@@ -3,8 +3,6 @@ var timeNdate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
 // writes that created variable to the html
 $("#todaysdatetime").text(timeNdate);
 
-
-
 // function to update the above timeNdate variable every second
 var update = function() {
     document.getElementById("todaysdatetime").innerHTML = moment().format("dddd, MMMM do YYYY, h:mm:ss a")
@@ -14,37 +12,39 @@ setInterval(update, 1000)
 
 // start a jquery function
 $(document).ready(function() {
+    
     // function to make info save to local storage when the save button is pressed
     // listens for click on the save button to run the function
-    // sets variables for time and value for the info entered by user
+    // sets variables for time and value for the info entered by user by going into the parent and sibling divs and text area
     // saves them to local storage
     $(".saveBtn").on("click", function() {
     var value = $(this).siblings(".information").val()
     var time = $(this).parent().attr("id")
     localStorage.setItem(time, value)
-    console.log(value)
-
-    hourColors()
     })
-
-    // set a constant that encompasses all rows in the html for all the business hours
-    // since all arrays 
-    // const rows = document.getElementsByClassName("row");
-// sets currentHour to the local time hour with moment js api
-    let currentHour = 11 //moment().hours();
     
-    function hourColors() {
-        for (let i = 9; i < 18; i++) {
-            const element = document.getElementById(`${i}`)
+    // run the hourColors function
+    colorTheHours()
+});    
+
+// sets currentHour to the local time hour with moment js api
+let currentHour = moment().hours();
+
+// function to run through the hours by ID, which is 9-17 in 24 hour time
+// based on the starting time of 9
+// adds a class based on the current cycled hour row against the real current time var set above this function    
+function colorTheHours() {
+    for (let i = 9; i < 18; i++) {
+        const blockedTime = document.getElementById(`${i}`)
             if (currentHour > i) {
-                $(element).addClass("past")
+                $(blockedTime).addClass("past")
             } else if (currentHour < i) {
-                $(element).addClass("future")
+                $(blockedTime).addClass("future")
             } else {
-                $(element).addClass("present")
+                $(blockedTime).addClass("present")
             }
-        }
     }
+}
 
     // pulls any local storage to populare the planner when it is refreshed or re-opened
 // this information is populated by the save button earlier in the script
@@ -57,4 +57,3 @@ $("#14 .information").val(localStorage.getItem("14"))
 $("#15 .information").val(localStorage.getItem("15"))
 $("#16 .information").val(localStorage.getItem("16"))
 $("#17 .information").val(localStorage.getItem("17"))
-});
